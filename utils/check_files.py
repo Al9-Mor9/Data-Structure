747ed6a81f19bf9ea5ac68d38c764b1f6256eb9d
ParkYeongSeo <74289372+Frog-Slayer@users.noreply.github.com>
2023-01-31 09:16:58 +0900

    Add) 풀이한 사람의 프로필을 누르면 해당 코드로 이동하도록 추가

--- utils/check_files.py
+++ utils/check_files.py
@@ -3,36 +3,41 @@ import os
 def get_code_list():
     code_dirs = [file for file in os.listdir("./Code/") if os.path.isdir(os.path.join("./Code", file))]
     code_list = {}
+    solver_list = {}
     for dir in code_dirs:
         codes = [file for file in os.listdir("./Code/"+dir) if os.path.isfile(os.path.join("./Code/"+dir, file))]
         for code in codes:
             tup = os.path.splitext(code)
             solution = tup[0].split('_')
             if len(solution) == 2 and len(solution[1]) == 1:
-                if dir in code_list:
-                    code_list[dir].append(solution[1])
+                code_list[(dir, solution[1])] = code
+                if dir in solver_list:
+                    solver_list[dir].append(solution[1])
                 else:
-                    code_list[dir] = []
-                    code_list[dir].append(solution[1])
-    return code_list
+                    solver_list[dir] = []
+                    solver_list[dir].append(solution[1])
+
+    return code_list, solver_list
   
 
 def update_readme(newReadme, profile):
-    code_list = get_code_list()
+    code_list, solver_list = get_code_list()
     for idx in range(len(newReadme)):
         line = newReadme[idx]
-        for problem_number in code_list:
+        for problem_number in solver_list:
             if problem_number in line:
-                for author in code_list[problem_number]:
+                for author in solver_list[problem_number]:
                     if profile[author] in line: 
                         continue
                     else :
-                        line = line.rstrip() + profile[author]
+                        line = line.rstrip() + make_link(profile[author], code_list[(problem_number, author)]) 
                         line += '\n'
 
         newReadme[idx] = line
     return newReadme
 
+def make_link(profile_url, code_path):
+    return '[' + profile_url +'](' + code_path + ')'
 
 if __name__ == "__main__":
     profile = {

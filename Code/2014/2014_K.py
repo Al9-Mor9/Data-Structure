f69af1c048cc592eba709101de0902a97a8869d0
Haaarimmm <haaarimmm@gmail.com>
2023-02-21 18:04:18 +0900

    K

---
+++ Code/2014/2014_K.py
@@ -0,0 +1,23 @@
+from queue import PriorityQueue
+import sys
+sys.stdin = open('input.txt', 'r')
+
+input = sys.stdin.readline
+
+K, N = map(int, input().split())
+prime = list(map(int, input().split()))
+
+q = PriorityQueue()     # 우선순위 큐
+for num in prime:
+    q.put(num)          # 주어진 소수들 넣기
+
+for i in range(N):      # 큐에서 N번 빼면 그 때의 값이 N번째 값
+    num = q.get()
+    for j in range(K):  # 뺀 값에 소수를 곱해서 push
+        new_num = num * prime[j]
+        q.put(new_num)
+
+        if num % prime[j] == 0: # 2 * 3 / 3 * 2 중복제거
+            break
+else:
+    print(num)

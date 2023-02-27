1663d2e30a5d8a8420db6d1002f64e5ab25526fa
wocjs <wocjs602@gmail.com>
2023-02-27 22:57:00 +0900

    hjc 13913_H

--- Code/13913/13913_H.py
+++ Code/13913/13913_H.py
@@ -6,21 +6,25 @@ def pathfind(now):
     data = []
     tmp = now
     for _ in range(visited[now] + 1):   # 경로 역추적
-        data.append(tmp)
+        data.append(tmp)    # 새로운 리스트 만들어서 append
         tmp = check[tmp]
     for i in range(len(data)):
-        print(data.pop(), end=' ')
+        print(data.pop(), end=' ')  # 걍 pop 반복,,
 
 
 n, m = map(int, input().split())
-visited = [0] * 100001
-check = [0] * 100001
+# 최댓값 저장
+visited = [0] * 100001  # 방문 확인 & 걸리는 시간 저장
+check = [0] * 100001    # 출처 번호 기록용
+
+# bfs
 q = [n]
 while q:
     num = q.pop(0)
-    if num == m:
+    if num == m:    # 탈출조건
         ans = visited[num]
         break
+    # n-1, n+1, n*2 append
     if 0 <= num-1 < len(visited) and not visited[num - 1]:
         q.append(num - 1)
         visited[num - 1] = visited[num] + 1
@@ -33,9 +37,10 @@ while q:
         q.append(num * 2)
         visited[num*2] = visited[num] + 1
         check[num * 2] = num
+
+# visited에 0으로 고정을 못함 그래서 루프 나와서 0으로 고정
 visited[n] = 0
 check[n] = 0
-# print(check[:30])
-# print(visited[:30])
 print(ans)
+# 경로 추적 레쓰고
 pathfind(num)
